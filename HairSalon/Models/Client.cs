f52184ed7f3cc6ed43d0d042b52712a01a42d451
noahkise <noah@kisefamily.com>
2024-02-03 13:08:55 -0800

    add imageURL capability to both forms and database tables

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,6 +4,7 @@ namespace HairSalon.Models
     {
         public int ClientId { get; set; }
         public string Name { get; set; }
+        public string ImageUrl { get; set; }
         public int StylistId { get; set; }
         public Stylist Stylist { get; set; }
     }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,6 +7,7 @@ namespace HairSalon.Models
     {
         public int StylistId { get; set; }
         public string Name { get; set; }
+        public string ImageUrl { get; set; }
         public List<Client> Clients { get; set; }
     }
 }
